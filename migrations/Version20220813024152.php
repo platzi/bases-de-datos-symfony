2e4d804555f83ae8fd33053741edbb378f201ffe
Italo Morales F <profesoritalomoralesf@gmail.com>
2022-08-12 21:57:19 -0500

    conceptos

--- migrations/Version20220813024152.php
+++ migrations/Version20220813024152.php
@@ -14,7 +14,7 @@ final class Version20220813024152 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Initial configuration (four tables)';
     }
 
     public function up(Schema $schema): void
