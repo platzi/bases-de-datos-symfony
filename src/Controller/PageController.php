00d10e9978a1aa0982acd7631affd31335c38808
Italo Morales F <profesoritalomoralesf@gmail.com>
2022-08-14 13:55:00 -0500

    plantilla web

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -9,10 +9,8 @@ use Symfony\Component\Routing\Annotation\Route;
 class PageController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function home(): Response
     {
-        return $this->render('page/index.html.twig', [
-            'controller_name' => 'PageController',
-        ]);
+        return $this->render('page/home.html.twig');
     }
 }
