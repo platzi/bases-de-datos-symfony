72c3ddd01860a354eb7f0e281b4af8c76e6a4aea
Italo Morales F <profesoritalomoralesf@gmail.com>
2022-08-14 14:12:46 -0500

    listado de registros

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +12,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class PageController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function home(): Response
+    public function home(EntityManagerInterface $entityManager): Response
     {
-        return $this->render('page/home.html.twig');
+        return $this->render('page/home.html.twig', [
+            'products' => $entityManager->getRepository(Product::class)->findAll()
+        ]);
     }
 }
